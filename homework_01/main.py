9e5d8ede4b98a6c55896cf8e475b944d8dc33150
Ali <alirashov998icloud.com@MacBook-Air-Ali.Dlink>
2022-05-04 00:02:45 +0500

    Добавляет первую домашку

---
+++ homework_01/main.py
@@ -0,0 +1,60 @@
+"""
+Домашнее задание №1
+Функции и структуры данных
+"""
+
+
+def power_numbers(*numbers):
+    """
+    функция, которая принимает N целых чисел,
+    и возвращает список квадратов этих чисел
+    >>> powers(1, 2, 5, 7)
+    <<< [1, 4, 25, 49]
+    """
+    powers = []
+    for number in numbers:
+        powers.append(pow(number, 2))
+
+    return powers
+
+
+# print(power_numbers(1, 2, 5, 7))
+
+
+
+# filter types
+ODD = "odd"
+EVEN = "even"
+PRIME = "prime"
+
+
+def filter_numbers(numbers, mode):
+    """
+    функция, которая на вход принимает список из целых чисел,
+    и возвращает только чётные/нечётные/простые числа
+    (выбор производится передачей дополнительного аргумента)
+
+    >>> filter_numbers([1, 2, 3], ODD)
+    <<< [1, 3]
+    >>> filter_numbers([2, 3, 4, 5], EVEN)
+    <<< [2, 4]
+    """
+    new_numbers = []
+    for number in numbers:
+        remainder = number % 2
+        if remainder != 0 and mode == ODD:
+            new_numbers.append(number)
+        elif remainder == 0 and mode == EVEN:
+            new_numbers.append(number)
+    if mode == PRIME:
+        new_numbers = list(filter(is_prime, numbers))
+    return new_numbers
+
+
+def is_prime(in_num):
+    if in_num % 2 == 0:
+        return in_num == 2
+    d = 3
+    while d * d <= in_num and in_num % d != 0:
+        d += 2
+    return d * d > in_num
---
+++ main.py
@@ -0,0 +1,5 @@
+from homework_01.main import filter_numbers, power_numbers, ODD, EVEN, PRIME
+
+# res = power_numbers(1, 2, 5)
+res = filter_numbers([1, 2, 4, 5], PRIME)
+print(res)
